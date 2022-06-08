4ae0bdf854f496628db6c6ecae8a8e7e2f586da3
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2022-06-08 13:03:31 +0100

    Add project files.

---
+++ WebDriveTests/WikipediaTest.cs
@@ -0,0 +1,26 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+//Create new Chrome browser instance
+var driver = new ChromeDriver();
+
+//navigate wikepadia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE:" + driver.Title);
+
+//Locate Search Fild by Id
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//click on element
+searchField.Click();
+
+//fill QA and press ENTER keybord button
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH  " + driver.Title);
+//close browser
+driver.Quit();
