f3051005b8ee9380ae9e4c850c7a86da16d04b22
Velina Chakarova <65416281+velichakarova@users.noreply.github.com>
2022-06-08 14:41:50 +0100

    WebDriver Tests(SoftUni)

---
+++ NunitWebDriverTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace NunitWebDriverTests
+{
+    public class SoftUniTests
+    {
+        private WebDriver driver;
+
+        [SetUp]
+        public void OpenBrowserNavigate()
+        {  
+            this.driver = new ChromeDriver();
+            driver.Url = "http://softuni.bg";
+            driver.Manage().Window.FullScreen();
+        }
+
+        [TearDown]
+        public void SchutDown()
+        {
+            driver.Quit();
+        }
+       
+
+        [Test]
+        public void Test_AssertMainPageTitle()
+        {
+            //Act      
+            string expectedTitile = "Обучение по програмиране - Софтуерен университет";
+            
+            //Assert
+            Assert.That(driver.Title, Is.EqualTo(expectedTitile));
+
+        }
+
+        [Test]
+        public void Test_AssertPage_AboutUs()
+        {
+            //Act
+            var zaNasField =driver.FindElement(By.CssSelector("#header-nav > div.toggle-nav.toggle-holder > ul > li:nth-child(1) > a > span"));
+            zaNasField.Click();
+            
+            string expectedTitile = "За нас - Софтуерен университет";
+
+            //Assert
+            Assert.That(driver.Title, Is.EqualTo(expectedTitile));
+
+        }
+    }
+}
